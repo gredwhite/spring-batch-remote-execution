b5e6f5b31d51bc7043ec552ddc9ef560e9102eba
ntkachev <Nikolay.Tkachev@dataart.com>
2019-08-14 15:32:16 +0300

    added examples for remote partitioning

---
+++ src/main/java/my/batch/experiments/remote/partitioning/aggregating/master/MasterApplication.java
@@ -0,0 +1,33 @@
+package my.batch.experiments.remote.partitioning.aggregating.master;
+
+import my.batch.experiments.remote.partitioning.BasicPartitioner;
+import my.batch.experiments.remote.partitioning.BrokerConfiguration;
+import org.apache.activemq.broker.BrokerService;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+import javax.annotation.PostConstruct;
+
+@EnableBatchProcessing
+@SpringBootApplication
+@Import({BasicPartitioner.class, BrokerConfiguration.class})
+public class MasterApplication {
+
+    @Value("${broker.url}")
+    private String brokerUrl;
+
+    public static void main(String[] args) {
+        SpringApplication.run(MasterApplication.class, args);
+    }
+
+
+    @PostConstruct
+    public void init() throws Exception {
+        BrokerService broker = new BrokerService();
+        broker.addConnector(brokerUrl);
+        broker.start();
+    }
+}
---
+++ src/main/java/my/batch/experiments/remote/partitioning/aggregating/worker/WorkerApplication.java
@@ -0,0 +1,17 @@
+package my.batch.experiments.remote.partitioning.aggregating.worker;
+
+import my.batch.experiments.remote.partitioning.BrokerConfiguration;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+@EnableBatchProcessing
+@SpringBootApplication
+@Import({BrokerConfiguration.class})
+
+public class WorkerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WorkerApplication.class, args);
+    }
+}
